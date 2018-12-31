94107eef49524ed3b86d10722a88d45cf24ebbee
Taiyaki <45515964+AsanoTaiki@users.noreply.github.com>
2019-01-01 03:23:51 +0900

    シェルスクリプト例外処理修正

--- tic-tac-toe.sh
+++ tic-tac-toe.sh
@@ -7,15 +7,21 @@ echo "   hard        -> Enter \"3\".";
 echo -n "Number : ";
 read DIFF
 
-if [ $DIFF = 1 ];then
-   KEY=1000
-elif [ $DIFF = 2 ];then
-   KEY=10000
-elif [ $DIFF = 3 ];then
-   KEY=500000
-else
-    echo "Input error!! : please enter correct number."
-fi
+while :
+do
+   if [ $DIFF = 1 ];then
+      KEY=1000
+      break
+   elif [ $DIFF = 2 ];then
+      KEY=10000
+      break
+   elif [ $DIFF = 3 ];then
+      KEY=500000
+      break
+   else
+      echo "Input error!! : please enter correct number."
+   fi
+done
 
 echo "Choose whether you are the first move or second move.";
 echo "   First move  -> Enter \"1\".";
@@ -23,12 +29,17 @@ echo "   Second move -> Enter \"2\".";
 echo -n "Number : ";
 read MOVE
 
-if [ $MOVE = 1 ];then
-    ./main 0 $KEY
-elif [ $MOVE = 2 ];then
-    ./main 1 $KEY
-else
-    echo "Input error!! : please enter correct number."
-fi
+while:
+do
+   if [ $MOVE = 1 ];then
+      ./main 0 $KEY
+      break
+   elif [ $MOVE = 2 ];then
+      ./main 1 $KEY
+      break
+   else
+      echo "Input error!! : please enter correct number."
+   fi
+done
 
 exit 0
