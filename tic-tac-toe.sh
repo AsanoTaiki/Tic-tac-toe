8fc4fab0560c675a04136b88c488d88069940c19
Taiyaki <45515964+AsanoTaiki@users.noreply.github.com>
2019-01-01 03:57:30 +0900

    シェルスクリプト修正

--- tic-tac-toe.sh
+++ tic-tac-toe.sh
@@ -1,45 +1,72 @@
 #!/bin/bash
 
-echo "Select the difficulty level.";
-echo "   easy        -> Enter \"1\".";
-echo "   normal      -> Enter \"2\".";
-echo "   hard        -> Enter \"3\".";
-echo -n "Number : ";
-read DIFF
+clear
 
 while :
 do
-   if [ $DIFF = 1 ];then
-      KEY=1000
-      break
-   elif [ $DIFF = 2 ];then
-      KEY=10000
-      break
-   elif [ $DIFF = 3 ];then
-      KEY=500000
-      break
-   else
-      echo "Input error!! : please enter correct number."
-   fi
+    echo "--------Tic_Tac_Toe_Game-------"
+    echo "Select the difficulty level.";
+    echo "   easy        -> Enter \"1\".";
+    echo "   normal      -> Enter \"2\".";
+    echo "   hard        -> Enter \"3\".";
+    echo -n "Number : ";
+    read DIFF
+
+    if [ $DIFF = 1 ];then
+	KEY=1000
+	KEYCHAR="easy"
+	break
+    elif [ $DIFF = 2 ];then
+	KEY=10000
+	KEYCHAR="normal"
+	break
+    elif [ $DIFF = 3 ];then
+	KEY=500000
+	KEYCHAR="hard"
+	break
+    else
+	echo "Input error!! : please enter correct number."
+	clear
+    fi
 done
 
-echo "Choose whether you are the first move or second move.";
-echo "   First move  -> Enter \"1\".";
-echo "   Second move -> Enter \"2\".";
-echo -n "Number : ";
-read MOVE
+clear
 
-while:
+while :
 do
-   if [ $MOVE = 1 ];then
-      ./main 0 $KEY
-      break
-   elif [ $MOVE = 2 ];then
-      ./main 1 $KEY
-      break
-   else
-      echo "Input error!! : please enter correct number."
-   fi
+    echo "--------Tic_Tac_Toe_Game-------"
+    echo "Choose whether you are the first move or second move.";
+    echo "   First move  -> Enter \"1\".";
+    echo "   Second move -> Enter \"2\".";
+    echo -n "Number : ";
+    read MOVE
+
+    if [ $MOVE = 1 ];then
+	MOVECHAR="First"
+	break
+    elif [ $MOVE = 2 ];then
+	MOVECHAR="Second"
+	break
+    else
+	echo "Input error!! : please enter correct number."
+	clear
+    fi
 done
+
+clear
+
+echo "--------Tic_Tac_Toe_Game-------"
+echo "Difficulty : $KEYCHAR";
+echo "Move : $MOVECHAR";
+echo "OK? [y/n]"
+read START
+
+if [ $START = "y" ];then
+    make
+    clear
+    ./main `expr $MOVE - 1` $KEY
+else
+    clear
+fi
 
 exit 0
